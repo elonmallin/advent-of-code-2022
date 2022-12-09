7fb2a53e07baa8d6fb980b8e1fd22c460088b8db
Elon Mallin <elon.mallin@nordax.se>
2022-12-09 15:51:00 +0100

    feat: day9 part1

--- day09/part1.ps1
+++ day09/part1.ps1
@@ -1,8 +1,9 @@
-$data = Get-Content -Path "$PSScriptRoot/input.example.txt"
+$data = Get-Content -Path "$PSScriptRoot/input.txt"
 
 $directionMap = @{ R=@(1,0); D=@(0,1); L=@(-1,0); U=@(0,-1) }
 $head = @(0, 0)
 $tail = @(0, 0)
+$tailVisits = @{}
 
 foreach ($line in $data) {
     $d, $c = $line -split " "
@@ -12,7 +13,22 @@ foreach ($line in $data) {
     foreach ($_ in 1..$c) {
         $head[0] += $d[0]
         $head[1] += $d[1]
+
+        foreach ($adj in @(@(1,0), @(-1,0), @(0,1), @(0,-1))) {
+            if ($adj[0] -ne 0 -and ($tail[0] + $adj[0] * 2) -eq $head[0]) {
+                $tail[0] += $adj[0]
+                $tail[1] = $head[1]
+                break
+            }
+            if ($adj[1] -ne 0 -and ($tail[1] + $adj[1] * 2) -eq $head[1]) {
+                $tail[0] = $head[0]
+                $tail[1] += $adj[1]
+                break
+            }
+        }
+
+        $tailVisits["$($tail[0]):$($tail[1])"] = $true
     }
 }
 
-$head
+$tailVisits.Keys.Count
