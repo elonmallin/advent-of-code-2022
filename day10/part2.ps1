1b91a0bf0e76d0a849d26023db3f544a0c92f671
Elon Mallin <elon.mallin@nordax.se>
2022-12-10 22:39:21 +0100

    feat: day10 part2

--- day10/part2.ps1
+++ day10/part2.ps1
@@ -1,32 +1,34 @@
 $data = Get-Content -Path "$PSScriptRoot/input.txt"
 
-$signalStrength = 0
 $cycleCount = 0
 $x = 1
 $instructions = @{}
-$cycleCheckpoint = 20
-$op = {
+$cycleCheckpoint = 40
+$cycleOp = {
     if ($cycleCheckpoint -eq $cycleCount) {
         $cycleCheckpoint += 40
-        $signalStrength += $x * $cycleCount
     }
 
     if ($instructions.ContainsKey($cycleCount)) {
         $x += $instructions[$cycleCount]
     }
 
+    $row = [Math]::Floor(($cycleCount)/40)
+    [string]$image[$row] += (($x-1)..($x+1)).Contains($cycleCount%40) ? "#" : "."
+
     $cycleCount += 1
 }
+$image = 1..6 | % { ,@() }
 
 foreach ($line in $data) {
-    . $op
+    . $cycleOp
 
     if ($line -ne "noop") {
         [int]$addX = ($line -split " ")[1]
         $instructions[$cycleCount+1] = $addX
         
-        . $op
+        . $cycleOp
     }
 }
 
-$signalStrength
+$image
