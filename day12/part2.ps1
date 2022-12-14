9df37fb6681ebbe9759b564766ef0a0ff3eb03ac
Elon Mallin <elon.mallin@gmail.com>
2022-12-15 00:06:20 +0100

    chore: cleanup day12

--- day12/part2.ps1
+++ day12/part2.ps1
@@ -1,40 +1,51 @@
 $data = Get-Content -Path "$PSScriptRoot/input.txt"
-
-$data = $data | ForEach-Object { ,($_.ToCharArray() | ForEach-Object { [int]$_ }) }
+$data = $data | % { ,($_.ToCharArray() | % { [int]$_ }) }
 
 function Build-Graph {
     param (
         $Data
     )
 
+    function Test-OutOfBounds {
+        param ($HeightMap, [int]$X, [int]$Y)
+        return ($X -lt 0 -or $Y -lt 0 -or $Y -ge $HeightMap.Count -or $X -ge $HeightMap[0].Count)
+    }
+    function Test-StepWithinReach {
+        param ([int]$Height, [int]$NextHeight)
+        return ($Height -ge $NextHeight -or $Height + 1 -ge $NextHeight)
+    }
+    $tryReplace = {param($data,$x,$y,$c)
+        if ($data[$y][$x] -eq [int][char]$c) {
+            $data[$y][$x] = @{S=[int][char]'a';E=[int][char]'z'}[$c]
+            return $true
+        }
+        return $false
+    }
+
     $graph = @{}
-    $s = ""
-    $e = ""
+    $start, $end = "", ""
 
     for ($y = 0; $y -lt $Data.Count; $y++) {
         for ($x = 0; $x -lt $Data[$y].Count; $x++) {
-            $graph["$x`:$y"] = @{ height=$Data[$y][$x]; items=[System.Collections.Generic.List[hashtable]]@() }
-            $dir = @(@(1,0),@(0,1),@(-1,0),@(0,-1))
-            
-            $h = $Data[$y][$x]
-            if ($h -eq [int][char]'S') { $h = [int][char]'a'; $s = "$x`:$y" }
-            if ($h -eq [int][char]'E') { $h = [int][char]'z'; $e = "$x`:$y" }
 
-            foreach ($d in $dir) {
-                $x2 = $x + $d[0]
-                $y2 = $y + $d[1]
+            if (&$tryReplace $Data $x $y 'S') { $start = "$x`:$y" }
+            if (&$tryReplace $Data $x $y 'E') { $end = "$x`:$y" }
+
+            $graph["$x`:$y"] = @{
+                height=$Data[$y][$x];
+                items=[System.Collections.Generic.List[hashtable]]@()
+            }
+            
+            foreach ($d in @(@(1,0),@(0,1),@(-1,0),@(0,-1))) {
+                $x2, $y2 = ($x + $d[0]), ($y + $d[1])
 
                 if (Test-OutOfBounds -HeightMap $Data -X $x2 -Y $y2) {
                     continue
                 }
 
-                $h2 = $Data[$y2][$x2]
-                if ($h2 -eq [int][char]'S') { $h2 = [int][char]'a' }
-                if ($h2 -eq [int][char]'E') {
-                    $h2 = [int][char]'z'
-                }
-
-                if (-not (Test-StepWithinReach -Height $h -NextHeight $h2)) {
+                if (&$tryReplace $Data $x2 $y2 'S') { $start = "$x2`:$y2" }
+                if (&$tryReplace $Data $x2 $y2 'E') { $end = "$x2`:$y2" }
+                if (-not (Test-StepWithinReach -Height $Data[$y][$x] -NextHeight $Data[$y2][$x2])) {
                     continue
                 }
 
@@ -43,34 +54,8 @@ function Build-Graph {
         }
     }
 
-    return $graph, $s, $e
+    return $graph, $start, $end
 }
-
-function Test-OutOfBounds {
-    param (
-        $HeightMap,
-        [int]$X,
-        [int]$Y
-    )
-    
-    return ($X -lt 0 -or $Y -lt 0 -or $Y -ge $HeightMap.Count -or $X -ge $HeightMap[0].Count)
-}
-
-function Test-StepWithinReach {
-    param (
-        [int]$Height,
-        [int]$NextHeight
-    )
-
-    if ($NextHeight -eq [int][char]'E') {
-        $NextHeight = [int][char]'z'
-    }
-    
-    return ($Height -ge $NextHeight -or $Height + 1 -ge $NextHeight)
-}
-
-$graph, $s, $e = Build-Graph -Data $data
-
 
 function Dijkstra {
     param (
@@ -109,29 +94,26 @@ function Dijkstra {
         }
     }
 
-    return $parentsMap, $nodeCosts
+    return $parentsMap
 }
 
+$graph, $start, $end = Build-Graph -Data $data
 $starts = $graph.Keys | Where-Object { $graph[$_].height -eq [int][char]'a' }
-$starts += $s
-$shortestStart = $null
-$shortestCount = [int]::MaxValue
-foreach ($st in $starts) {
-    
-    $par, $nc = Dijkstra -Graph $graph -Start $st
-    $k = $par[$e]
-    $i = 0
-    while ($k) {
-        $i++
-        $k = $par[$k]
-        if ($k -eq $s) {
-            break
-        }
-    }
 
-    if ($shortestCount -gt $i -and $i -gt 0) {
+$shortestCount = [int]::MaxValue
+foreach ($s in $starts) {
+    $map = Dijkstra -Graph $graph -Start $s
+    $k = $end
+    $i = 0
+
+    do {
+        $i++
+        $k = $map[$k]
+    }
+    while ($k -and $k -ne $s)
+
+    if ($shortestCount -gt $i -and $i -gt 1) {
         $shortestCount = $i
-        $shortestStart = $st
     }
 }
 
