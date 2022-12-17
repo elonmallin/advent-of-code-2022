bb023ec4a3efc309333f5c46a0b13217424e66c2
Elon Mallin <elon.mallin@gmail.com>
2022-12-17 19:30:09 +0100

    feat: day13 part1

--- day13/part1.ps1
+++ day13/part1.ps1
@@ -1,33 +1,48 @@
-$data = Get-Content -Path "$PSScriptRoot/input.example.txt" -Raw
+$data = Get-Content -Path "$PSScriptRoot/input.txt" -Raw
 
 $pairs = $data -split "`r?`n`r?`n" | % { ,($_ -split "`r?`n") }
 
-$compareAtIdx = {param($a,$b,$i)$a[$i] -lt $b[$i]}
-
 function Test-Order {
-    param (
-        $l,
-        $r,
-        $li,
-        $ri
-    )
+    param ($l, $r)
     
-    if ($l[$li] -is [int] -and $r[$ri] -is [int] -and $l[$li] -gt $r[$ri]) {
-        return $false # int, left is higher than right
-    }
-
-
-    return $true
-}
-
-$rightOrderCount = 0
-foreach ($pair in $pairs) {
-    $l, $r = $pair | % { ,(Invoke-Expression (($_ -replace "\[", "@(") -replace "\]", ")")) }
-
-    $j = 0
-    for ($i = 0; $i -lt $l.Count; $i++) {
-        if ($l[$i] -is [int] -and $r[$j] -is [int] -and $l[$i] -gt $r[$j]) {
-            break # int, left is higher than right
+    if ($l -is [array] -and $r -is [array]) {
+        $i = 0
+        while ($i -lt $l.Count -and $i -lt $r.Count) {
+            $c = Test-Order $l[$i] $r[$i]
+            if ($c -eq 1) {
+                return 1
+            }
+            elseif ($c -eq -1) {
+                return -1
+            }
+            $i++
         }
+
+        if ($i -eq $l.Count -and $i -lt $r.Count) {
+            return -1
+        }
+        elseif ($i -eq $r.Count -and $i -lt $l.Count) {
+            return 1
+        }
+
+        return 0
+    }
+    elseif ($l -is [int64] -and $r -is [int64]) {
+        return $l -eq $r ? 0 : ($l -lt $r ? -1 : 1)
+    }
+
+    return Test-Order ($l -is [int64] ? ,$l : $l) ($r -is [int64] ? ,$r : $r) 0 0
+}
+
+$i = 0
+$sum = 0
+foreach ($pair in $pairs) {
+    $i++
+    $l, $r = $pair[0], $pair[1] | % { $_ -replace "`r?`n", "" | ConvertFrom-Json -NoEnumerate }
+
+    if ((Test-Order $l $r) -eq -1) {
+        $sum += $i
     }
 }
+
+$sum
