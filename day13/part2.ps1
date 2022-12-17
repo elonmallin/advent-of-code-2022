dc5e4b4380a29691fcebaccba7314a5dc4dd56d6
Elon Mallin <elon.mallin@gmail.com>
2022-12-17 20:01:12 +0100

    feat: day13 almost part2 powershell is really not good for this

---
+++ day13/part2.ps1
@@ -0,0 +1,56 @@
+$data = Get-Content -Path "$PSScriptRoot/input.txt"
+
+[System.Collections.Generic.List[object]]$pairs = $data | Where-Object { $_ }
+$pairs.Add("[[2]]")
+$pairs.Add("[[6]]")
+$pairs = $pairs | % { $_ | ConvertFrom-Json -NoEnumerate }
+
+function Test-Order {
+    param ($l, $r)
+    
+    if ($l -is [array] -and $r -is [array]) {
+        $i = 0
+        while ($i -lt $l.Count -and $i -lt $r.Count) {
+            $c = Test-Order $l[$i] $r[$i]
+            if ($c -eq 1) {
+                return 1
+            }
+            elseif ($c -eq -1) {
+                return -1
+            }
+            $i++
+        }
+
+        if ($i -eq $l.Count -and $i -lt $r.Count) {
+            return -1
+        }
+        elseif ($i -eq $r.Count -and $i -lt $l.Count) {
+            return 1
+        }
+
+        return 0
+    }
+    elseif ($l -is [int64] -and $r -is [int64]) {
+        return $l -eq $r ? 0 : ($l -lt $r ? -1 : 1)
+    }
+
+    return Test-Order ($l -is [int64] ? ,$l : $l) ($r -is [int64] ? ,$r : $r) 0 0
+}
+
+$pairs.Sort({
+    param($a,$b)
+    Test-Order $a $b
+})
+
+$res = 1
+for ($i = 0; $i -lt $pairs.Count; $i++) {
+    if (Compare-Object $pairs[$i] @(@(2)) -IncludeEqual -ExcludeDifferent) {
+        $res *= $i+1
+    }
+    if (Compare-Object $pairs[$i] @(@(6)) -IncludeEqual -ExcludeDifferent) {
+        $res *= $i+1
+    }
+}
+
+$res
+
