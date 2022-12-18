60b7ce9dfacb4054cb782335f11115f4a5a1d9d4
Elon Mallin <elon.mallin@gmail.com>
2022-12-18 10:52:47 +0100

    feat: day14 part1

--- day14/part1.ps1
+++ day14/part1.ps1
@@ -1,10 +1,10 @@
-$data = Get-Content -Path "$PSScriptRoot/input.example.txt"
+$data = Get-Content -Path "$PSScriptRoot/input.txt"
 
 $lines = $data | % { ,($_ -split " -> " | % { ,($_ -split "," | % { [int]$_ }) }) }
 
 $cave = @{}
 
-function Format-Cave {
+function Get-CaveRect {
     [CmdletBinding()]
     param (
         [parameter(ValueFromPipeline, Position=0)]$Cave
@@ -14,6 +14,17 @@ function Format-Cave {
     $ys = $Cave.Keys | % { ($_ -split ":")[1] | % { [int]$_ } } | Sort-Object
     $xMin, $xMax = $xs[0], $xs[-1]
     $yMin, $yMax = $ys[0], $ys[-1]
+
+    return $xMin, $yMin, $xMax, $yMax
+}
+
+function Format-Cave {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipeline, Position=0)]$Cave
+    )
+
+    $xMin, $yMin, $xMax, $yMax = Get-CaveRect $Cave
 
     $formattedCave = ""
     for ($y = $yMin; $y -le $yMax; $y++) {
@@ -43,17 +54,48 @@ function Write-RockLine {
     }
 }
 
+function Add-Sand {
+    param (
+        $Cave,
+        $SpawnPoint,
+        $CaveRect,
+        $Moves = @(@(0,1),@(-1,1),@(1,1))
+    )
+    
+    $xMin, $yMin, $xMax, $yMax = $CaveRect
+    $p = $SpawnPoint
+    do {
+        foreach ($d in $Moves) {
+            $x2, $y2 = ($p[0] + $d[0]), ($p[1] + $d[1])
+            if ($x2 -lt $xMin -or $x2 -gt $xMax -or $y2 -gt $yMax) {
+                return $false, @($x2,$y2)
+            }
+            if (-not $Cave["$x2`:$y2"]) {
+                $p[0] = $x2
+                $p[1] = $y2
+                break
+            }
+        }
+    } while ($p[0] -eq $x2 -and $p[1] -eq $y2)
+
+    $Cave["$($p[0]):$($p[1])"] = "o"
+
+    return $true, $p
+}
+
 foreach ($line in $lines) {
     $p = $line[0]
     foreach ($coord in $line[1..($line.Count-1)]) {
         Write-RockLine $cave $p $coord
         $p = $coord
-
-        # Clear-Host
-        # $cave | Format-Cave | Write-Host -NoNewline
-        # Start-Sleep -Milliseconds 300
-        
     }
 }
 
+$caveRect = Get-CaveRect $Cave
+do {
+    $settled, $point = Add-Sand $Cave @(500,0) $caveRect
+} while($settled)
+
 $cave | Format-Cave
+
+($cave.Values | Where-Object { $_ -eq "o" }).Count
