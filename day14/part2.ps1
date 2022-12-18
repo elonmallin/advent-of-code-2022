e36894e0ae92dabff25fd70cab452a58e4789a48
Elon Mallin <elon.mallin@gmail.com>
2022-12-18 11:10:39 +0100

    feat: day14 part2

---
+++ day14/part2.ps1
@@ -0,0 +1,105 @@
+$data = Get-Content -Path "$PSScriptRoot/input.txt"
+
+$lines = $data | % { ,($_ -split " -> " | % { ,($_ -split "," | % { [int]$_ }) }) }
+
+$cave = @{}
+
+function Get-CaveRect {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipeline, Position=0)]$Cave
+    )
+    
+    $xs = $Cave.Keys | % { ($_ -split ":")[0] | % { [int]$_ } } | Sort-Object
+    $ys = $Cave.Keys | % { ($_ -split ":")[1] | % { [int]$_ } } | Sort-Object
+    $xMin, $xMax = $xs[0], $xs[-1]
+    $yMin, $yMax = $ys[0], $ys[-1]
+
+    return $xMin, $yMin, $xMax, $yMax
+}
+
+function Format-Cave {
+    [CmdletBinding()]
+    param (
+        [parameter(ValueFromPipeline, Position=0)]$Cave
+    )
+
+    $xMin, $yMin, $xMax, $yMax = Get-CaveRect $Cave
+
+    $formattedCave = ""
+    for ($y = $yMin; $y -le $yMax; $y++) {
+        for ($x = $xMin; $x -le $xMax; $x++) {
+            $formattedCave += ($Cave["$x`:$y"] ? $Cave["$x`:$y"] : ".")
+        }
+        $formattedCave += "`n"
+    }
+
+    return $formattedCave
+}
+
+function Write-RockLine {
+    param ($Cave, $P1, $P2)
+    
+    $d = @(
+        ($P1[0] -eq $P2[0] ? 0 : ($P2[0] -lt $P1[0] ? -1 : 1)),
+        ($P1[1] -eq $P2[1] ? 0 : ($P2[1] -lt $P1[1] ? -1 : 1))
+    )
+
+    $p = @($P1[0], $P1[1])
+    $Cave["$($p[0]):$($p[1])"] = "#"
+    while ($p[0] -ne $P2[0] -or $p[1] -ne $P2[1]) {
+        $p[0] += $d[0]
+        $p[1] += $d[1]
+        $Cave["$($p[0]):$($p[1])"] = "#"
+    }
+}
+
+function Add-Sand {
+    param (
+        $Cave,
+        $SpawnPoint,
+        $CaveRect,
+        $Moves = @(@(0,1),@(-1,1),@(1,1))
+    )
+    
+    $xMin, $yMin, $xMax, $yMax = $CaveRect
+    $p = $SpawnPoint
+    do {
+        foreach ($d in $Moves) {
+            $x2, $y2 = ($p[0] + $d[0]), ($p[1] + $d[1])
+            if ($y2 -eq ($yMax + 2)) {
+                $Cave["$($p[0]):$($p[1])"] = "o"
+
+                return $p
+            }
+            if (-not $Cave["$x2`:$y2"]) {
+                $p[0] = $x2
+                $p[1] = $y2
+                break
+            }
+        }
+    } while ($p[0] -eq $x2 -and $p[1] -eq $y2)
+
+    $Cave["$($p[0]):$($p[1])"] = "o"
+
+    return $p
+}
+
+foreach ($line in $lines) {
+    $p = $line[0]
+    foreach ($coord in $line[1..($line.Count-1)]) {
+        Write-RockLine $cave $p $coord
+        $p = $coord
+    }
+}
+
+$caveRect = Get-CaveRect $Cave
+do {
+    $point = Add-Sand $Cave @(500,0) $caveRect
+} while($point[0] -ne 500 -or $point[1] -ne 0)
+
+$caveRect = Get-CaveRect $Cave
+Write-RockLine $cave @(($caveRect[0]-2),($caveRect[3]+1)) @(($caveRect[2]+2),($caveRect[3]+1))
+$cave | Format-Cave
+
+($cave.Values | Where-Object { $_ -eq "o" }).Count
