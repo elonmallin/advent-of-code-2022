e1dce684c7350b5290595de5a0fcb7317a456e2b
Elon Mallin <elon.mallin@gmail.com>
2022-12-18 15:35:44 +0100

    feat: day15 part1 parse input wip

--- day15/part1.ps1
+++ day15/part1.ps1
@@ -1,1 +1,10 @@
 $data = Get-Content -Path "$PSScriptRoot/input.example.txt"
+
+$sensorsAndBeacons = $data | % {
+    $s, $b = $_ -split ":"
+    $sx, $sy = ($s | Select-String -Pattern "x=(-?\d+), y=(-?\d+)").Matches.Groups.Value | Select-Object -Skip 1
+    $bx, $by = ($b | Select-String -Pattern "x=(-?\d+), y=(-?\d+)").Matches.Groups.Value | Select-Object -Skip 1
+
+    return ,@(@($sx, $sy), @($bx, $by))
+}
+$sensorsAndBeacons
